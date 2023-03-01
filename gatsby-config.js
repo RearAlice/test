91ecffad0e84f14a537317e3d21963ec80a56498
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-01 16:59:32 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,20 @@ module.exports = {
         display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          // Footnotes mode (default: true)
+          footnotes: true,
+          // GitHub Flavored Markdown mode (default: true)
+          gfm: true,
+          // Add your gatsby-remark-* plugins here
+          plugins: [],
+          // Enable JS for https://github.com/jonschlinkert/gray-matter#optionsengines (default: false)
+          // It's not advised to set this to "true" and this option will likely be removed in the future
+          jsFrontmatterEngine: false,
+        },
+      },
     },
     `gatsby-plugin-gatsby-cloud`,
   ],
